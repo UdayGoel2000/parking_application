66a4fced9204bcad38ef934bc138dab42edd3b53
UD <udaygoel2000@gmail.com>
2024-05-12 22:52:24 +0530

    Create NavBar.tsx

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,9 @@
+import style from "./NavBar.module.css";
+const NavBar = () => {
+  return (
+    <div className={style.NavBar}>
+      <h1>Parking Aplication</h1>
+    </div>
+  );
+};
+export default NavBar;
